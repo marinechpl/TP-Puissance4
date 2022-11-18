087eb6b0c6fafcfece46cb62abb7a78592811cff
Marine Chapelle <marine.chapelle@epfedu.fr>
2022-11-18 12:19:40 +0100

    création classes + jetons fini

--- src/tp/puissance4/PlateauDeJeu.java
+++ src/tp/puissance4/PlateauDeJeu.java
@@ -9,5 +9,18 @@ package tp.puissance4;
  * @author Elisa
  */
 public class PlateauDeJeu {
+    CelluleDeGrille[][] grille = new CelluleDeGrille[6][7];
+
+    public PlateauDeJeu() {
+        for (int i=0; i<6;i++){
+            for (int j=0; j<7; j++){
+                grille[i][j] = grille[6][7];
+            }            
+        }   
+    }
     
+    public int ajouterJetonDansColonne(String jeton, int indice){
+    
+    }
+   
 }
